0fe3009429f4db21ef689528fdd0fa70c83915bf
Ricardo Abreu <rjga94@gmail.com>
2020-12-19 22:17:18 +0000

    Project setup

---
+++ Assets/_Project/Input/InputMaster.cs
@@ -0,0 +1,67 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/_Project/Input/InputMaster.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+namespace Input
+{
+    public class @InputMaster : IInputActionCollection, IDisposable
+    {
+        public InputActionAsset asset { get; }
+        public @InputMaster()
+        {
+            asset = InputActionAsset.FromJson(@"{
+    ""name"": ""InputMaster"",
+    ""maps"": [],
+    ""controlSchemes"": []
+}");
+        }
+
+        public void Dispose()
+        {
+            UnityEngine.Object.Destroy(asset);
+        }
+
+        public InputBinding? bindingMask
+        {
+            get => asset.bindingMask;
+            set => asset.bindingMask = value;
+        }
+
+        public ReadOnlyArray<InputDevice>? devices
+        {
+            get => asset.devices;
+            set => asset.devices = value;
+        }
+
+        public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+        public bool Contains(InputAction action)
+        {
+            return asset.Contains(action);
+        }
+
+        public IEnumerator<InputAction> GetEnumerator()
+        {
+            return asset.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void Enable()
+        {
+            asset.Enable();
+        }
+
+        public void Disable()
+        {
+            asset.Disable();
+        }
+    }
+}
