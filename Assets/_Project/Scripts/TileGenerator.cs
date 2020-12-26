a0db2bd709c850209de29b4f6c0df96e51e1ac00
Ricardo Abreu <rjga94@gmail.com>
2020-12-26 21:39:58 +0000

    Added a procedural generated level using tilemaps

---
+++ Assets/_Project/Scripts/TileGenerator.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using Random = UnityEngine.Random;
+
+public class TileGenerator : MonoBehaviour
+{
+    [SerializeField] private Tilemap tilemap;
+    [SerializeField] private TileBase groundTile;
+    [SerializeField] private TileBase groundTopTile;
+
+    [SerializeField] private int width, height, seed, minSectionWidth;
+
+    private void Start()
+    {
+        RenderMap(RandomWalkTopSmoothed(GenerateArray(width, height, true), seed, minSectionWidth), tilemap, groundTile);
+    }
+
+    public static int[,] GenerateArray(int width, int height, bool empty)
+    {
+        int[,] map = new int[width, height];
+        for (int x = 0; x < map.GetUpperBound(0); x++)
+        {
+            for (int y = 0; y < map.GetUpperBound(1); y++)
+            {
+                if (empty)
+                {
+                    map[x, y] = 0;
+                }
+                else
+                {
+                    map[x, y] = 1;
+                }
+            }
+        }
+        return map;
+    }
+    
+    public static void RenderMap(int[,] map, Tilemap tilemap, TileBase tile)
+    {
+        //Clear the map (ensures we dont overlap)
+        tilemap.ClearAllTiles(); 
+        //Loop through the width of the map
+        for (int x = 0; x < map.GetUpperBound(0) ; x++) 
+        {
+            //Loop through the height of the map
+            for (int y = 0; y < map.GetUpperBound(1); y++) 
+            {
+                // 1 = tile, 0 = no tile
+                if (map[x, y] == 1) 
+                {
+                    tilemap.SetTile(new Vector3Int(x, y, 0), tile); 
+                }
+            }
+        }
+    }
+    
+    public static int[,] RandomWalkTopSmoothed(int[,] map, float seed, int minSectionWidth)
+    {
+        //Seed our random
+        System.Random rand = new System.Random(seed.GetHashCode());
+
+        //Determine the start position
+        int lastHeight = Random.Range(0, map.GetUpperBound(1));
+
+        //Used to determine which direction to go
+        int nextMove = 0;
+        //Used to keep track of the current sections width
+        int sectionWidth = 0;
+
+        //Work through the array width
+        for (int x = 0; x <= map.GetUpperBound(0); x++)
+        {
+            //Determine the next move
+            nextMove = rand.Next(2);
+
+            //Only change the height if we have used the current height more than the minimum required section width
+            if (nextMove == 0 && lastHeight > 0 && sectionWidth > minSectionWidth)
+            {
+                lastHeight--;
+                sectionWidth = 0;
+            }
+            else if (nextMove == 1 && lastHeight < map.GetUpperBound(1) && sectionWidth > minSectionWidth)
+            {
+                lastHeight++;
+                sectionWidth = 0;
+            }
+            //Increment the section width
+            sectionWidth++;
+
+            //Work our way from the height down to 0
+            for (int y = lastHeight; y >= 0; y--)
+            {
+                map[x, y] = 1;
+            }
+        }
+
+        //Return the modified map
+        return map;
+    }
+}
