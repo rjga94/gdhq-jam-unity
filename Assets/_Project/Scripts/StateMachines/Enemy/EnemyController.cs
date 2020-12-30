120a489c288b1573ccad9b9eb40d2bf4d6d804ff
Ricardo Abreu <rjga94@gmail.com>
2020-12-30 20:08:31 +0000

    Started adding animations

--- Assets/_Project/Scripts/StateMachines/Enemy/EnemyController.cs
+++ Assets/_Project/Scripts/StateMachines/Enemy/EnemyController.cs
@@ -2,7 +2,7 @@
 
 namespace StateMachines.Enemy
 {
-    [RequireComponent(typeof(Rigidbody2D))]
+    [RequireComponent(typeof(Rigidbody2D)), RequireComponent(typeof(Animator))]
     public class EnemyController : StateMachine<EnemyController>
     {
         [SerializeField] public float movementSpeed;
@@ -10,9 +10,14 @@ namespace StateMachines.Enemy
         [SerializeField] public GameObject attackColliderGO;
 
         [HideInInspector] public Rigidbody2D Rigidbody2D;
+        [HideInInspector] public Animator Animator;
         [HideInInspector] public Collider2D target;
 
-        private void Awake() => Rigidbody2D = GetComponent<Rigidbody2D>();
+        private void Awake()
+        {
+            Rigidbody2D = GetComponent<Rigidbody2D>();
+            Animator = GetComponent<Animator>();
+        }
 
         private void Start() => SetState(new IdleState(this));
 
