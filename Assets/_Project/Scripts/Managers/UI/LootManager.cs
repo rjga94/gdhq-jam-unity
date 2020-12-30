e54734d4bedcfbec828feaf12a15c6500b30087e
masonwoodford <mwoodfor@ucsc.edu>
2020-12-30 13:06:20 -0800

    loot count

---
+++ Assets/_Project/Scripts/Managers/UI/LootManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using TMPro;
+using StateMachines.Player;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Managers
+{
+    public class LootManager : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text lootText;
+        [SerializeField] private PlayerController playerController;
+
+        private void Update()
+        {
+            lootText.text = playerController.collectedShards.ToString();
+        }
+    }
+}
